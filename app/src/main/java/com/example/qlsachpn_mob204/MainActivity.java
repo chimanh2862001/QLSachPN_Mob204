47dee98b5013d5f39f47de34ce84723c1b8dbce4
nguyenchimanh <manhxinhtrai30@gmail.com>
2020-11-16 09:56:38 +0700

    abc123

--- app/src/main/java/com/example/qlsachpn_mob204/MainActivity.java
+++ app/src/main/java/com/example/qlsachpn_mob204/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
        Button button10;
        List<NguoiDung> nguoiDungList;
        TextView textView8;
+       
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
